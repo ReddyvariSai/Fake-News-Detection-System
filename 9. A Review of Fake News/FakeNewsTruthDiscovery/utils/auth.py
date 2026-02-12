be9c6022d5870fdd73d6c22783f06b79f51c8134
Sai Kumar Reddy <rsaikumarreddy2004@gmail.com>
2026-02-12 17:26:57 +0530

    Delete 9. A Review of Fake News/FakeNewsTruthDiscovery directory

--- 9. A Review of Fake News/FakeNewsTruthDiscovery/utils/auth.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_password(password):
-    # Simple hashing for demonstration purposes
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def verify_password(stored_password, provided_password):
-    return stored_password == hashlib.sha256(provided_password.encode()).hexdigest()
